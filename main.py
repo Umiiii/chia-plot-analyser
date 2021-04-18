ec214db0c106fc83a9fcdc16473b35cb18ed0135
umi <9@umi.cat>
2021-04-19 00:15:27 +0800

    fix

--- main.py
+++ main.py
@@ -20,12 +20,11 @@ if __name__ == "__main__":
         colmn = ["logfile","plot_id","stage","phase1","phase2","phase3","phase4","total"]
         result = []
         for dirpath,dirnames,filenames in os.walk(PATH):
-            print(logfile)
             for filename in filenames:
                 logfile = os.path.join(dirpath,filename)
                 print(logfile)
                 j = Job(os.path.join(dirpath,logfile))
                 result.append([j.logfile, j.plot_id, j.phase, j.elapsed_time[0], j.elapsed_time[1], j.elapsed_time[2], j.elapsed_time[3], j.total_time])
             print(tabulate(result,colmn,tablefmt="fancy_grid"))
-        time.sleep(20)
+        time.sleep(5)
         cls()
